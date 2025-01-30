ac654e64bbdba2f031c987ad75f096e01bab77d2
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2025-01-30 16:50:58 +0000

    comet

---
+++ U7T1/UsedCarLot.java
@@ -0,0 +1,28 @@
+package U7T1;
+import java.util.ArrayList;
+
+public class UsedCarLot {
+    private ArrayList<Car> inventory;
+
+    public UsedCarLot() {
+        inventory = new ArrayList<Car>();
+    }
+
+    public ArrayList<Car> getInventory() {
+        return inventory;
+    }
+
+    public void addCar(Car car) {
+        inventory.add(car);
+    }
+
+    public boolean swap(int index1, int index2) {
+        if (index1 <= inventory.size() - 1 && index2 <= inventory.size() - 1) {
+            Car temp = inventory.get(index1);
+            inventory.set(index1, inventory.get(index2));
+            inventory.set(index2, temp);
+            return true;
+        }
+        return false;
+    }
+}
